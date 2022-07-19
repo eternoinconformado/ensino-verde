4b3535a40411be535301df9a4d8d63495e45709d
Eterno Inconformado <vitoramacedo@hotmail.com>
2022-07-19 00:11:57 -0300

    Add files via upload

---
+++ acordo-eco.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Acordos Ambientais - Ensino Verde</title>
+		<?php include("z-config.php") ?>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<?php include("z-header.php"); ?>
+
+							<!-- Content -->
+								<section>
+									<header class="main">
+										<h1>Protocolo de Kyoto e o Acordo de Paris</h1>
+										<h5>Fichamento por: Matheus Henrique | Núcleo Temático de Educação Ambiental Interdisciplinar, 2021.2</h5>
+									</header>
+
+									<span class="image main"><img src="images/ecologia1.jpg" alt="Banner Energia Renovavel" /></span>
+
+									<p>É possível observar os impactos ambientais causados pelas mudanças climáticas em grande escalas pelo mundo, tais como um aumento no número de incêndios florestais, elevação dos níveis e temperaturas dos oceanos, fenômenos climáticos incomuns e várias outras consequências que ocorrem com mais frequência nos últimos anos. Portanto, para tentar minimizar (ou até mesmo reverter) a mudança climática, vários países de todo o mundo discutem e tentam chegar a acordos para combater este problema, resultando então em um esforço global. </p>
+									<p>Graças a esses esforços, surgiram várias regulamentações e um aumento na supervisão para que as regulamentações sejam seguidas, além do surgimento de várias metas a serem alcançadas, tais como a substituição das matrizes energéticas de cada país, trocando as energias convencionais pelas energias renováveis. Dentre estes acordos, podemos citar o Protocolo de Kyoto e o Acordo de Paris.</p>
+
+									<
+
+									<hr class="major" />
+
+									<h2>Protocolo de Kyoto e o Crédito de Carbono</h2>
+									<span class="image main"><img src="images/Eco3.jpg" alt="Hidreletrica" /></span>
+									<p>O Protocolo de Kyoto foi um acordo mundial assinado por mais de 175 países em 1997 visando diminuir a emissão de gases do efeito estufa. Em 1999, 55 países que emitiam mais de 50% das emissões globais de gases do efeito estufa ratificaram o protocolo, ou seja, criaram leis nacionais para garantir seu cumprimento. Porém, esse acordo só entrou em vigor em 16 de fevereiro de 2004, após a ratificação por parte da Rússia </p>
+									<p>Este tratado possui várias metas, prazos e regulamentações para os países envolvidos, com o objetivo de reduzir os impactos ambientais causados pela emissão dos gases de efeito estufa. Para isto, foi necessário levar em consideração o contexto industrial e tecnológico destes países e, portanto, decidiu-se que os países desenvolvidos deveriam reduzir em 5,2% suas emissões de gases do efeito estufa (em especial o dióxido de carbono) em relação às emissões registradas em 1990, enquanto que os países em desenvolvimento, tal como o Brasil, não receberam metas e obrigações a serem cumpridas. Dos países desenvolvidos, o Japão e a União Européia  tiveram metas de redução de 7% e 8%, respectivamente. As metas dos países envolvidos deveriam ser alcançadas entre 2008 e 2012, sendo então definida outra meta de até 18% de redução até 2020, em relação a 1990.</p>
+                                    <p>Para diminuir as emissões de gases do efeito estufa e os impactos ambientais, o protocolo de Kyoto propôs mudanças nas matrizes energéticas e do setor de transporte, um maior investimento e uso de energias renováveis, proteção a florestas, combate ao desmatamento, sustentabilidade na agricultura e várias outras ações. Também propôs o investimento no desenvolvimento de tecnologias, bem como seu compartilhamento entre países.</p> 
+                                    <p>Para flexibilizar o cumprimento das metas propostas sem afetar o objetivo final do tratado, foi criado o mecanismo de desenvolvimento limpo (MDL), que certificou as emissões de cada país, possibilitando que alguns países que estejam com dificuldades de alcançar as suas metas possuam outras alternativas por meio de negociações. Surgiu então o “Crédito de Carbono”, que permite que países que ultrapassaram as suas metas de redução de emissões comercializem o “excesso” com países que não conseguiram alcançar as metas propostas, sendo este crédito de carbono “produzido” a cada tonelada de carbono não emitido na atmosfera. Desta forma, é incentivada a redução das emissões além da meta estabelecida, pois torna-se possível utilizá-la como moeda de negociação com os países que não conseguiram alcançá-la. Além disso, o MDL também propõe alternativas para os países que não alcançaram suas metas, tal como o desenvolvimento de projetos, em território nacional ou internacional, com impacto real que reduzam as emissões de carbono.</p> 
+                                    <p></p> 
+									<hr class="major" />
+
+									<h2>Acordo de Paris</h2>
+									<span class="image main"><img src="images/Eco2.jpg" alt="Energia Eolica" /></span>
+                                    <p>O Acordo de Paris é um acordo mundial, que previa substituir o Protocolo de Kyoto em 2020, com o objetivo de enfrentar as mudanças climáticas, em especial o aumento da temperatura global. Como ocorreu durante o Protocolo de Kyoto, 55 países que representavam mais de 50% das emissões globais de gases do efeito estufa precisam ratificar o tratado. O Acordo de Paris foi assinado por 195 países e, dentro destes, foi ratificado por 147 países, incluindo o Brasil, até junho de 2017, mas entrou em vigor em novembro de 2016. O cumprimento deste acordo é extremamente importante para impedir o progresso da mudança climática e seus impactos ambientais, pois o seu predecessor (Protocolo de Kyoto) falhou em alcançar suas metas, tendo em vista que poucos países alcançaram as metas propostas.</p> 
+                                    <p>De forma mais específica, este tratado tem como objetivo impedir o aumento de 2 ºC na temperatura global em relação aos níveis pré-industriais. Para isto, é proposto a criação de mecanismos para diminuir o impacto da mudança climática, bem como o aumento na utilização de fontes renováveis de energia. Diferentemente do protocolo de Kyoto, cada país participante formulou as suas Contribuições Nacionais Determinadas (CND), contendo seus planos e metas para sua colaboração para a meta global de redução. No final de 2020, esses países apresentaram suas CND e suas metas relativas ao ano anterior do acordo, ao invés de ser em relação ao período industrial. De 5 em 5 anos, estes países devem apresentar os avanços de suas CNDs, bem como seus resultados.</p>
+                                    <p>No caso do Brasil, foi decidido que sua CND principal seria a redução de gases do efeito estufa em 37% até 2025 e 43% até 2030, além do combate ao desmatamento, substituição das fontes convencionais pelas fontes renováveis, reflorestamento e uma reforma na matriz energética que favoreça o uso de fontes renováveis.</p>
+									<hr class="major" />
+
+
+																
+									
+
+								</section>
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+				<?php include("z-sidebar.php"); ?>
+
+			</div>
+
+		<!-- Scripts -->
+				<?php include("z-scripts.php"); ?>
+
+	</body>
+</html>
--- z-sidebar.php
+++ z-sidebar.php
@@ -4,59 +4,51 @@
 		<!-- Menu -->
 			<nav id="menu">
 				<header class="major">
-					<h2>Menu</h2>
+					<h2>Escolha um tema...</h2>
 				</header>
 				<ul>
-					<li><a href="index.php">Início</a></li>
+					<li><a href="index.php">Página Inicial</a></li>
 					<li>
 						<span class="opener active">Fontes de Energia</span>
 						<ul>
-							<li><a href="#">Energias Convencionais</a></li>
-							<li><a href="#">Energias Renováveis</a></li>
+							<li><a href="energia-convencional.php">Energias Convencionais</a></li> <!-- FALTA ARQUIVO -->
+							<li><a href="energia-renovavel.php">Energias Renováveis</a></li>
 						</ul>
 					</li>
 					<li>
 						<span class="opener active">Descarte de Lixo</span>
 						<ul>
-							<li><a href="coleta-seletiva.php">Coleta Seletiva e Reciclagem</a></li>
+							<li><a href="reciclagem.php">Reciclagem</a></li> <!-- FALTA ARQUIVO -->						
+							<li><a href="coleta-seletiva.php">Coleta Seletiva</a></li>
 							<li><a href="descartes-inadequados.php">Descartes Inadequados</a></li>
 							<li><a href="cinco-erres-da-sustentabilidade.php">Os Cinco Erres da Sustentabilidade</a></li>
 						</ul>
 					</li>
-					<li><a href="mudancas-climaticas.php">Mudanças Climáticas</a></li>					
-					<li><a href="#">Algum outro tema...</a></li>					
+					<li>
+						<span class="opener active">Impactos e Estratégias</span>
+						<ul>
+							<li><a href="mudancas-climaticas.php">Mudanças Climáticas</a></li>
+							<li><a href="acordos-eco.php">Acordos Ambientais</a></li>			
+							<li><a href="computacao-verde.php">Computação Verde</a></li>							
+						</ul>
+					</li>					
+					<li>
+						<span class="opener active">Jogos e Ferramentas</span>
+						<ul>
+							<li><a href="matematica-do-lixo.php">Quanto lixo tua cidade produz?</a></li>
+						</ul>
+					</li>
 				</ul>
 			</nav>
 
 		<!-- Section -->
 			<section>
 				<header class="major">
-					<h2>Jogos e Ferramentas</h2>
+					<h2>Contato com o PEV</h2>
 				</header>
-				<div class="mini-posts">
-					<article>
-						<a href="matematica-do-lixo.php" class="image"><img src="images/matematica-do-lixo.jpg"/></a>
-						<p>Quando lixo tua cidade produz?</p>
-					</article>
-					<article>
-						<a href="jogos/jogo2/" class="image"><img src="images/pic08.jpg" alt="" /></a>
-						<p>Jogo 2</p>
-					</article>
-					<article>
-						<a href="jogos/jogo3/" class="image"><img src="images/pic09.jpg" alt="" /></a>
-						<p>Jogo 3</p>
-					</article>
-				</div>
-			</section>
-
-		<!-- Section -->
-			<section>
-				<header class="major">
-					<h2>Sobre</h2>
-				</header>
-				<p>Página vinculada ao <a href="https://escolaverde.org/" target="_blank">Programa Escola Verde (PEV)</a> – um movimento social de mobilização e transformação pedagógica e colaborativa de escolas e comunidades no enfrentamento dos problemas socioambientais.</p>
 				<ul class="contact">
-					<li class="icon solid fa-envelope"><a href="mailto:escolaverde@univasf.edu.br">escolaverde@univasf.edu.br</a> / <a href="mailto:programaescolaverde@gmail.com">programaescolaverde@gmail.com</a></li>
+					<li class="icon solid fa-envelope"><a href="mailto:escolaverde@univasf.edu.br">escolaverde@univasf.edu.br</a></li>					
+					<li class="icon solid fa-envelope"><a href="mailto:programaescolaverde@gmail.com">programaescolaverde@gmail.com</a></li>
 					<li class="icon solid fa-phone"><a href="tel:07421027660">(74) 2102-7660</a></li>
 					<li class="icon solid fa-home">Universidade Federal do Vale do São Francisco | Av. Antônio Carlos Magalhães, 510, Country Club | Bloco de Colegiados - 2º andar - Sala Verde | CEP: 48.902-300 | Juazeiro/BA</li>
 				</ul>
@@ -64,7 +56,10 @@
 
 		<!-- Footer -->
 			<footer id="footer">
-				<p class="copyright">Elaborado por estudantes do Núcleo Temático de Educação Ambiental Interdisciplinar, 2021.2. | Modelo: <a href="https://html5up.net">HTML5 UP</a>.</p>
+				<p class="copyright"><strong style="color:green"><a href="index.php">Ensino Verde</a></strong> por Matheus Passos, Thalisson Castro e Vítor Fagundes
+				<br><br>
+				Modelo: <a href="https://html5up.net">HTML5 UP</a>
+				</p>
 			</footer>
 
 	</div>
