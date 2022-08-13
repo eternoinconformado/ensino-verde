53fc731b3015253affe17ad1299d2838e3bd4f6f
Eterno Inconformado <vitoramacedo@hotmail.com>
2022-08-13 14:12:52 -0300

    Add files via upload

---
+++ backups/z-sidebar.php
@@ -0,0 +1,65 @@
+<div id="sidebar">
+	<div class="inner">
+
+		<!-- Menu -->
+			<nav id="menu">
+				<header class="major">
+					<h2>Escolha um tema...</h2>
+				</header>
+				<ul>
+					<li><a href="index.php">Página Inicial</a></li>
+					<li>
+						<span class="opener active">Fontes de Energia</span>
+						<ul>
+							<li><a href="energias-convencionais.php">Energias Convencionais</a></li>
+							<li><a href="energia-renovavel.php">Energias Renováveis</a></li>
+						</ul>
+					</li>
+					<li>
+						<span class="opener active">Descarte de Lixo</span>
+						<ul>
+							<li><a href="coleta-seletiva.php">Coleta Seletiva e Reciclagem</a></li>
+							<li><a href="descartes-inadequados.php">Descartes Inadequados</a></li>
+							<li><a href="cinco-erres-da-sustentabilidade.php">Os Cinco Erres da Sustentabilidade</a></li>
+						</ul>
+					</li>
+					<li>
+						<span class="opener active">Impactos e Estratégias</span>
+						<ul>
+							<li><a href="mudancas-climaticas.php">Mudanças Climáticas</a></li>
+							<li><a href="acordo-eco.php">Acordos Ambientais</a></li>			
+							<li><a href="desenvolvimento-sustentavel.php">Desenvolvimento Sustentável</a></li>							
+							<li><a href="computacao-verde.php">Computação Verde</a></li>							
+						</ul>
+					</li>					
+					<li>
+						<span class="opener active">Jogos e Ferramentas</span>
+						<ul>
+							<li><a href="matematica-do-lixo.php">Quanto lixo tua cidade produz?</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+
+		<!-- Section -->
+			<section>
+				<header class="major">
+					<h2>Contato com o PEV</h2>
+				</header>
+				<ul class="contact">
+					<li class="icon solid fa-envelope"><a href="mailto:escolaverde@univasf.edu.br">escolaverde@univasf.edu.br</a></li>					
+					<li class="icon solid fa-envelope"><a href="mailto:programaescolaverde@gmail.com">programaescolaverde@gmail.com</a></li>
+					<li class="icon solid fa-phone"><a href="tel:07421027660">(74) 2102-7660</a></li>
+					<li class="icon solid fa-home">Universidade Federal do Vale do São Francisco | Av. Antônio Carlos Magalhães, 510, Country Club | Bloco de Colegiados - 2º andar - Sala Verde | CEP: 48.902-300 | Juazeiro/BA</li>
+				</ul>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<p class="copyright"><strong style="color:green"><a href="index.php">Ensino Verde</a></strong> por Matheus Passos, Thalisson Castro e Vítor Fagundes
+				<br><br>
+				Modelo: <a href="https://html5up.net">HTML5 UP</a>
+				</p>
+			</footer>
+
+	</div>
