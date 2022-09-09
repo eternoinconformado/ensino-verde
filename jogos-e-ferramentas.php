3ef5addab80f3d2085530c8cae7c4f2e285adc89
Eterno Inconformado <vitoramacedo@hotmail.com>
2022-09-09 15:16:22 -0300

    Add files via upload

--- jogos-e-ferramentas.php
+++ jogos-e-ferramentas.php
@@ -28,45 +28,35 @@
 							<a href="matematica-do-lixo.php" target="_blank" class="image"><img src="images/matematica-do-lixo.jpg" alt="Quanto lixo tua cidade produz?" /></a>
 							<h3>Quanto lixo tua cidade produz?</h3>
 							<p>Já parou para pensar na enorme quantidade de lixo que é produzida por dia na tua cidade e como isso impacta o meio ambiente? Acesse esta ferramenta para saber mais a respeito.</p><br>
-							<p class="creditos"><i><b>Autores: </b>Matheus Passos e Vítor Fagundes</i>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
-							<p>
+							<p class="creditos"><i><b>Autores: </b>Matheus Passos e Vítor Fagundes</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
 								<li><a href="matematica-do-lixo.php" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
 							</ul>
 						</article>
-						<article>
-							<a href="https://ambientados.netlify.app/index.html" target="_blank" class="image"><img src="images/ambientados.jpg" alt="Ambientados" /></a>
-							<h3>Ambientados</h3>
-							<p>Quiz com o intuito de proporcionar o conhecimento para as crianças sobre a importância que o meio ambiente exerce sobre a sociedade.</p>
-							<p class="creditos"><i><b>Autores: </b>André Golçalves, Edon Brendon, Jules Bomfim, Marcos Felipe e Paulo Lucas</i>
 
-							<p>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
-							<ul class="actions">
-								<li><a href="https://ambientados.netlify.app/index.html" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
-							</ul>
-						</article>
 						<article>
-							<a href="QUIZ-APP/index.html" target="_blank" class="image"><img src="QUIZ-APP/img/lamp.png" alt="QUIZ" /></a>
+							<a href="quiz" target="_blank" class="image"><img src="quiz/img/lamp.png" alt="QUIZ" /></a>
 							<h3>Ensino Verde - Quiz</h3>
 							<text>Quiz com o intuito de proporcionar o conhecimento sobre os assuntos abordados no site sobre meio ambiente.</text>
+							<br><br>
 							<h4>Instruções:</h4>
 							<ul>
 								<li>Leia a pergunta que irá aparecer na tela e selecione a alternativa que você julgar como correta.</li>
 								<li>Após clicar na alternativa, a resposta correta aparecerá em verde e as demais em vermelho.</li><br>
 							</ul>
-							<p class="creditos"><i><b>Autores: </b>Adão Thalisson e Matheus Passos</i>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
-							<p>
+							<p class="creditos"><i><b>Autores: </b>Adão Thalisson e Matheus Passos</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
-								<li><a href="QUIZ-APP/index.html" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
+								<li><a href="quiz" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
 							</ul>
 						</article>
+						
 						<article>
-							<a href="WasteSorting/index.html" target="_blank" class="image"><img src="images/coleta_seletiva.jpg" alt="coletaSeletiva" /></a>
+							<a href="WasteSorting" target="_blank" class="image"><img src="images/coleta_seletiva.jpg" alt="coletaSeletiva" /></a>
 							<h3>Coleta Seletiva</h3>
 							<text>Mini jogo com o intuito de proporcionar o conhecimento para as crianças e até adultos a memorizar as cores certas para cada tipo de resíduo.</text>
+							<br><br>
 							<h4>Instruções:</h4>
 							<ul>
 								<li>Capture o material recliclável de acordo com a cor respectiva do cesto para coleta seletiva.</li>
@@ -75,22 +65,28 @@
 									<p>Clique aqui para ler o conteúdo sobre coleta seletiva</p>
 								</a>
 							</ul>
-
-
-							<p class="creditos"><i><b>Autores: </b>Adão Thalisson e Matheus Passos</i>
-							<p>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
+							<p class="creditos"><i><b>Autores: </b>Adão Thalisson e Matheus Passos</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
-								<li><a href="WasteSorting/index.html" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
+								<li><a href="WasteSorting" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
 							</ul>
 						</article>
+						<article>
+							<a href="https://ambientados.netlify.app/index.html" target="_blank" class="image"><img src="images/ambientados.jpg" alt="Ambientados" /></a>
+							<h3>Ambientados</h3>
+							<p>Quiz com o intuito de proporcionar o conhecimento para as crianças sobre a importância que o meio ambiente exerce sobre a sociedade.</p>
+							<p class="creditos"><i><b>Autores: </b>André Golçalves, Edon Brendon, Jules Bomfim, Marcos Felipe e Paulo Lucas</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
+							<ul class="actions">
+								<li><a href="https://ambientados.netlify.app/index.html" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
+							</ul>
+						</article>						
 						<article>
 							<a href="https://escolaverde.org/pevpegada/#home" target="_blank" class="image"><img src="images/pev-pegada.png" alt="PEV Pegada" /></a>
 							<h3>PEV Pegada</h3>
 							<p>Site que explica o que é a pegada de carbono e traz outras informações relevantes sobre o tema, ajudando o usuário a reletir sobre a quantidade de área bioprodutiva necessária para suprir a demanda das pessoas por recursos naturais e para a absorção do carbono.</p>
-							<p class="creditos"><i><b>Autores: </b>Henrique França e Anderson Barros</i>
-							<p>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
+							<p class="creditos"><i><b>Autores: </b>Henrique França e Anderson Barros</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
 								<li><a href="https://escolaverde.org/pevpegada/#home" target="_blank" class="button leiaMais">Acessar (Versão Web)</a></li>
 							</ul>
@@ -99,9 +95,8 @@
 							<a href="https://escolaverde.org/site/?p=70464" target="_blank" class="image"><img src="images/pevgada-ecologica.png" alt="PEVgada Ecológica" /></a>
 							<h3>PEVgada Ecológica</h3>
 							<p>Aplicativo que explica o que é Pegada Ecológica e traz uma seção onde o usuário pode calcular sua Pegada Ecológica com o objetivo de estimular a reflexão para a exacerbação do nosso consumo no dia a dia.</p>
-							<p class="creditos"><i><b>Autores: </b>Fernando Bagagi, Gabriela Leite, Matheus Bispo, João Victor Leal e Ramon Gomes</i>
-							<p>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
+							<p class="creditos"><i><b>Autores: </b>Fernando Bagagi, Gabriela Leite, Matheus Bispo, João Victor Leal e Ramon Gomes</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
 								<li><a href="https://escolaverde.org/site/?p=70464" target="_blank" class="button leiaMais">Baixar (Android - APK)</a></li>
 							</ul>
@@ -110,9 +105,8 @@
 							<a href="https://escolaverde.org/site/?p=73962" target="_blank" class="image"><img src="images/biosolo.png" alt="BioSolo" /></a>
 							<h3>BioSolo</h3>
 							<p>Webjogo para educação ambiental de crianças que aborda a biodiversidade e a importância da preservação do solo para produção de alimentos e toda a vida no planeta Terra.</p>
-							<p class="creditos"><i><b>Autores: </b>Verônica Leal, Ricardo Ramos e Adson Layrton</i>
-							<p>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
+							<p class="creditos"><i><b>Autores: </b>Verônica Leal, Ricardo Ramos e Adson Layrton</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
 								<li><a href="https://escolaverde.org/site/?p=73962" target="_blank" class="button leiaMais">Baixar (Android - APK)</a></li>
 							</ul>
@@ -121,9 +115,8 @@
 							<a href="https://play.google.com/store/apps/details?id=com.pev.www" target="_blank" class="image"><img src="images/app-georreferenciamento.png" alt="Georreferenciamento de Escolas Visitadas no Vale do São Francisco" /></a>
 							<h3>Georreferenciamento de Escolas Visitadas no Vale do São Francisco</h3>
 							<p>Aplicativo de georreferenciamento das escolas envolvidas no Projeto Escola Verde.</p>
-							<p class="creditos"><i><b>Autores: </b>Luan Fonseca e Washington Azevedo (Big Eyes Solutions)</i>
-							<p>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
+							<p class="creditos"><i><b>Autores: </b>Luan Fonseca e Washington Azevedo (Big Eyes Solutions)</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
 								<li><a href="https://play.google.com/store/apps/details?id=com.pev.www" target="_blank" class="button leiaMais">Baixar (Android - Play Store)</a></li>
 							</ul>
@@ -132,9 +125,8 @@
 							<a href="https://escolaverde.org/site/?p=42049" target="_blank" class="image"><img src="images/cd-rom.png" alt="CD-ROM do PEV" /></a>
 							<h3>CD-ROM do PEV</h3>
 							<p>Material didático que pretende auxiliar os professores de diferentes áreas e disciplinas à introduzir a temática socioambiental em sala de aula, a partir de seus conteúdos disciplinares.</p>
-							<p class="creditos"><i><b>Autores: </b>Leonardo Campello, Ramon Carvalho e colaboradores</i>
-							<p>
-							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i>
+							<p class="creditos"><i><b>Autores: </b>Leonardo Campello, Ramon Carvalho e colaboradores</i></p>
+							<p class="creditos"><i><b>Orientador: </b>Prof. Paulo Roberto Ramos</i></p>
 							<ul class="actions">
 								<li><a href="https://escolaverde.org/site/?p=42049" target="_blank" class="button leiaMais">Baixar (Windows)</a></li>
 							</ul>
